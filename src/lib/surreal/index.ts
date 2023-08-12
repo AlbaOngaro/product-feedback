53ed9c7ea16563fe39cdf57d72b3f28f272c66ca
Alba Ongaro <alba.ongaro@reprisk.com>
2023-08-13 00:14:00 +0200

    chore: added node-version

--- src/lib/surreal/index.ts
+++ src/lib/surreal/index.ts
@@ -2,5 +2,5 @@ import { Surreal, ExperimentalSurrealHTTP } from "surrealdb.js";
 
 export const surreal =
   process.env.NEXT_RUNTIME === "edge"
-    ? new ExperimentalSurrealHTTP("http://127.0.0.1:8000/rpc")
-    : new Surreal("http://127.0.0.1:8000/rpc");
+    ? new ExperimentalSurrealHTTP(process.env.SURREALDB_HOST)
+    : new Surreal(process.env.SURREALDB_HOST);
